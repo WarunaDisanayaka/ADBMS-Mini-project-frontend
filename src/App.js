a60db0986ba2f0791b7bfbcf1cd244e8f1eda710
ravinduWP <104779889+ravinduWP@users.noreply.github.com>
2023-10-21 12:27:59 +0530

    dashboard creation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Signup from "./components/Signup";
+import Dashboard from "./components/Admin/dashboard";
 function App() {
   return (
     
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>}></Route>
         <Route path="/signup" element={<Signup/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
       </Routes>
     </BrowserRouter>
 

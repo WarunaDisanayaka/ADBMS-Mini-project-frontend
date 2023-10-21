8e12cb7f62715a80d0fdee18d55c9cfcbfcbc60e
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2023-10-21 13:17:12 +0530

    Add Assets

---
+++ src/components/Admin/addassets.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function addassets() {
+  return (
+    <div className="login template d-flex justify-content-center align-items-center vh-100 bg-light">
+        <div className='form_container shadow p-5 mb-5  rounded bg-white'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default addassets

86ac7f3af30f37d10467f68388b2ba288948a282
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2023-10-28 12:55:56 +0530

    update asstes

--- src/components/Admin/Assets/add_assets.js
+++ src/components/Admin/Assets/add_assets.js
@@ -17,28 +17,48 @@ function AddAssets() {
                         <h3 className='text-center'>Add Assets</h3>
                         <div class="mb-3 mt-3">
                             <label for='assets id' class='from-lable'>Assets ID: </label>
-                            <label input type='text' class='from-control' id='assets id' placeholder='Enter assets ID' name='assets id'></label>
+                            <div class="col-sm-10">
+                            <input type='text' class='from-control' id='assets id' name='assets id'/>
+                            </div>
                         </div>
                         <div class="mb-3 mt-3">
-                            <label for='name' class='from-lable'>Name: </label>
-                            <label input type='text' class='from-control' id='name' placeholder='Enter Name' name='name'></label>
+                            <label for='name' class='from-lable'>Assets Name: </label>
+                            <div class="col-sm-10">
+                            <input type='text' class='from-control' id='name' name='name'/>
+                            </div>
                         </div>
                         <div class="mb-3 mt-3">
                             <label for='status' class='from-lable'>Status: </label>
+                            <div class="col-sm-10">
                             <select className='from select mt-3'>
                                 <option>Use Assets</option>
                                 <option>Repair Assets</option>
                                 <option>Out of use Assets</option>
                             </select>
+                            </div>
                         </div>
                         <div class="mb-3 mt-3">
                             <label for='adddate' class='from-lable'>Added Date: </label>
-                            <label input type='date' class='from-control' id='add' placeholder='Enter Added Date' name='adddate'></label>
+                            <div class="col-sm-10">
+                            <input type='date' class='from-control' id='add' placeholder='Enter Added Date' name='adddate'/>
+                            </div>
                         </div>
                         <div>
                         <label for='addtime' class='from-lable'>Time: </label>
-                        <label input type='time' class='from-control' id='add' placeholder='Enter time' name='addtime'></label>
+                        <div class="col-sm-10">
+                        <input type='time' class='from-control' id='add' placeholder='Enter time' name='addtime'/>
                         </div>
+                        </div>
+                        <div class="mb-3">
+                        <div class="mb-3"></div>
+                        <div class="col-sm-10">
+                        <input class="correct-input" type="checkbox" id="Check"/>
+                            <label class="addcorrect" for="Check">
+                            All are Correct
+                            </label>
+                        </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
                         </form>
                     </div>
                 </div>

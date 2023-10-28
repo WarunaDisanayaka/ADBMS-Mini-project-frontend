6310c7a718da34c7ed3b2cb42fb074ab36d6bc3a
ravinduWP <104779889+ravinduWP@users.noreply.github.com>
2023-10-28 18:46:42 +0530

    room registering form creation

--- src/components/Admin/Rooms/add_rooms.js
+++ src/components/Admin/Rooms/add_rooms.js
@@ -1,19 +1,100 @@
 import React from 'react';
 import Sidebar from '../Sidebar';
 import Topbar from '../../Topbar';
+import { Link } from 'react-router-dom';
+import { Form } from 'react-bootstrap';
 
 function AddRooms() {
     return (
-        <div className='d-flex'>
+        <div className='d-flex '>
             <div>
-                <Sidebar/>
+                <Sidebar />
             </div>
             <div className='flex-grow-1'>
-                <Topbar/>
-                <div className="p-4">
-                    <h2>Add Rooms Page</h2>
-                    <p>This is the Add Rooms page content. You can add your content here.</p>
+                <Topbar />
+
+                <div className='container p-5 mb-5 mx-auto mt-5 rounded bg-light shadow' style={{ width: 600 }}>
+                    <form >
+                        <h3 className='text-center mb-4'>Register a Room</h3>
+                        <div className='row justify-content-md-center'>
+                            <div className="col-4 mb-2 p-1">
+                                <label htmlFor='username' className=''>Hostel  </label>
+                            </div>
+
+                            <div className='col-6 mb-2 p-1'>
+
+                                <Form.Select>
+                                <option disabled selected value="">select the Hostel</option>
+                                    <option value="g">Girl's hostel</option>
+                                    <option value="b">Boy's hostel</option>
+
+                                </Form.Select>
+                            </div>
+                        </div>
+                        <div className='row justify-content-md-center'>
+                            <div className="col-4 mb-2 p-1">
+                                <label htmlFor='username' className=''>Floor</label>
+                            </div>
+
+                            <div className='col-6 mb-2 p-1'>
+                                <Form.Select >
+                                <option disabled selected value="">select the Floor</option>
+                                    <option value="0">Ground Floor</option>
+                                    <option value="1">First Floor</option>
+                                    <option value="2">Second Floor</option>
+                                    <option value="3">Third Floor</option>
+
+                                </Form.Select>
+                            </div>
+                        </div>
+                        <div className='row justify-content-md-center'>
+                            <div className="col-4 mb-2 p-1">
+                                <label htmlFor='username' className=''>Room Type</label>
+                            </div>
+
+                            <div className='col-6 mb-2 p-1'>
+
+                                <Form.Select>
+                                    <option disabled selected value="">select the room type</option>
+                                    <option value="0">Bedroom</option>
+                                    <option value="1">Washroom</option>
+                                    <option value="2">Common Room</option>
+                                </Form.Select>
+                            </div>
+                        </div>
+                        <div className='row justify-content-md-center'>
+                            <div className="col-4 mb-2 p-1">
+                                <label htmlFor='username' className=''>Room Number </label>
+                            </div>
+
+                            <div className='col-6 mb-2 p-1'>
+
+                                <input type='number' placeholder='Enter Room Number' className='form-control' />
+                            </div>
+                        </div>
+                        <div className='row justify-content-md-center'>
+                            <div className="col-4 mb-2 p-1">
+                                <label htmlFor='username' className=''>Number of Students </label>
+                            </div>
+
+                            <div className='col-6 mb-2 p-1'>
+
+                                <input type='number' placeholder='Enter Number of Students' className='form-control' />
+                            </div>
+                        </div>
+
+                        <div className="row justify-content-end">
+                            <div className='col-2 mb-2 p-1'>
+                            <button type='submit' className="btn btn-outline-primary">Register</button>
+                            </div>
+                            <div className='col-3 mb-2 p-1'>
+                            <button type='submit' className="btn btn-outline-secondary">Cancel</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
+
+
             </div>
         </div>
     );

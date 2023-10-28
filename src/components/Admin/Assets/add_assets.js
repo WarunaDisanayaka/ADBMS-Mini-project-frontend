92c5d80461ae458ce4b67a31b940a66b2158c6c1
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2023-10-28 11:29:21 +0530

    new assets

--- src/components/Admin/Assets/add_assets.js
+++ src/components/Admin/Assets/add_assets.js
@@ -11,8 +11,37 @@ function AddAssets() {
             <div className='flex-grow-1'>
                 <Topbar/>
                 <div className="p-4">
-                    <h2>Add Assets Page</h2>
-                    <p>This is the Add Assets page content. You can add your content here.</p>
+                <div className="login template d-flex justify-content-center align-items-center vh-100 bg-light">
+                    <div className='form_container shadow p-5 mb-5  rounded bg-white'>
+                        <form>
+                        <h3 className='text-center'>Add Assets</h3>
+                        <div class="mb-3 mt-3">
+                            <label for='assets id' class='from-lable'>Assets ID: </label>
+                            <label input type='text' class='from-control' id='assets id' placeholder='Enter assets ID' name='assets id'></label>
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label for='name' class='from-lable'>Name: </label>
+                            <label input type='text' class='from-control' id='name' placeholder='Enter Name' name='name'></label>
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label for='status' class='from-lable'>Status: </label>
+                            <select className='from select mt-3'>
+                                <option>Use Assets</option>
+                                <option>Repair Assets</option>
+                                <option>Out of use Assets</option>
+                            </select>
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label for='adddate' class='from-lable'>Added Date: </label>
+                            <label input type='date' class='from-control' id='add' placeholder='Enter Added Date' name='adddate'></label>
+                        </div>
+                        <div>
+                        <label for='addtime' class='from-lable'>Time: </label>
+                        <label input type='time' class='from-control' id='add' placeholder='Enter time' name='addtime'></label>
+                        </div>
+                        </form>
+                    </div>
+                </div>
                 </div>
             </div>
         </div>

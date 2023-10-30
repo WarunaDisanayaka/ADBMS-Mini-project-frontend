6fcf9c557d9244e7d37a9bd69d65005a3dea899e
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2023-10-30 23:34:37 +0530

    add bootstrap

--- src/components/Admin/Assets/add_assets.js
+++ src/components/Admin/Assets/add_assets.js
@@ -11,57 +11,57 @@ function AddAssets() {
             <div className='flex-grow-1'>
                 <Topbar/>
                 <div className="p-4">
-                <div className="login template d-flex">
-                    <div className='form_container'>
-                        <form>
-                        <h3 className='text-center'>Add Assets</h3>
-                        <div class="mb-3 mt-3">
-                            <label for='assets id' class='from-lable'>Assets ID: </label>
-                            <div class="col-sm-10">
-                            <input type='text' class='from-control' id='assets id' name='assets id'/>
-                            </div>
+                    <div className="login template d-flex">
+                        <div className='form_container'>
+                            <form>
+                                <h3 className='text-center'>Add Assets</h3>
+                                <div className="mb-3 mt-3">
+                                    <label htmlFor='assetsId' className='form-label'>Assets ID: </label>
+                                    <div className="col-sm-10">
+                                        <input type='text' className='form-control' id='assetsId' name='assetsId'/>
+                                    </div>
+                                </div>
+                                <div className="mb-3 mt-3">
+                                    <label htmlFor='name' className='form-label'>Assets Name: </label>
+                                    <div className="col-sm-10">
+                                        <input type='text' className='form-control' id='name' name='name'/>
+                                    </div>
+                                </div>
+                                <div className="mb-3 mt-3">
+                                    <label htmlFor='status' className='form-label'>Status: </label>
+                                    <div className="col-sm-10">
+                                        <select className='form-select mt-3'>
+                                            <option>Use Assets</option>
+                                            <option>Repair Assets</option>
+                                            <option>Out of use Assets</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="mb-3 mt-3">
+                                    <label htmlFor='addDate' className='form-label'>Added Date: </label>
+                                    <div className="col-sm-10">
+                                        <input type='date' className='form-control' id='addDate' placeholder='Enter Added Date' name='addDate'/>
+                                    </div>
+                                </div>
+                                <div>
+                                    <label htmlFor='addTime' className='form-label'>Time: </label>
+                                    <div className="col-sm-10">
+                                        <input type='time' className='form-control' id='addTime' placeholder='Enter time' name='addTime'/>
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <div className="mb-3"></div>
+                                    <div className="col-sm-10">
+                                        <input className="form-check-input" type="checkbox" id="Check"/>
+                                        <label className="form-check-label" htmlFor="Check">
+                                            All are Correct
+                                        </label>
+                                    </div>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Submit</button>
+                            </form>
                         </div>
-                        <div class="mb-3 mt-3">
-                            <label for='name' class='from-lable'>Assets Name: </label>
-                            <div class="col-sm-10">
-                            <input type='text' class='from-control' id='name' name='name'/>
-                            </div>
-                        </div>
-                        <div class="mb-3 mt-3">
-                            <label for='status' class='from-lable'>Status: </label>
-                            <div class="col-sm-10">
-                            <select className='from select mt-3'>
-                                <option>Use Assets</option>
-                                <option>Repair Assets</option>
-                                <option>Out of use Assets</option>
-                            </select>
-                            </div>
-                        </div>
-                        <div class="mb-3 mt-3">
-                            <label for='adddate' class='from-lable'>Added Date: </label>
-                            <div class="col-sm-10">
-                            <input type='date' class='from-control' id='add' placeholder='Enter Added Date' name='adddate'/>
-                            </div>
-                        </div>
-                        <div>
-                        <label for='addtime' class='from-lable'>Time: </label>
-                        <div class="col-sm-10">
-                        <input type='time' class='from-control' id='add' placeholder='Enter time' name='addtime'/>
-                        </div>
-                        </div>
-                        <div class="mb-3">
-                        <div class="mb-3"></div>
-                        <div class="col-sm-10">
-                        <input class="correct-input" type="checkbox" id="Check"/>
-                            <label class="addcorrect" for="Check">
-                            All are Correct
-                            </label>
-                        </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
                     </div>
-                </div>
                 </div>
             </div>
         </div>

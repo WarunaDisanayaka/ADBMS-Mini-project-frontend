ae96f00bfcf79187a5af4f69253ec3a5ebb0b8f7
Tharukee Kiriella <tharukee99@gmail.com>
2023-10-30 12:58:26 +0530

    add view table

--- src/components/Admin/Assets/view_assets.js
+++ src/components/Admin/Assets/view_assets.js
@@ -1,18 +1,98 @@
 import React from 'react';
 import Sidebar from '../Sidebar';
 import Topbar from '../../Topbar';
+import { Table } from 'react-bootstrap';
 
 function ViewAssets() {
     return (
         <div className='d-flex'>
             <div>
-                <Sidebar/>
+                <Sidebar />
             </div>
             <div className='flex-grow-1'>
-                <Topbar/>
+                <Topbar />
                 <div className="p-4">
-                    <h2>View Assets Page</h2>
-                    <p>This is the View Assets page content. You can add your content here.</p>
+
+
+
+                    <h2><center>Display Assets</center></h2>
+
+
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Asset ID</th>
+                                <th>Asset Name</th>
+                                <th>Asset Image</th>
+                                <th>More Details</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>0001</td>
+                                <td>Bunk bed</td>
+                                <td></td>
+                                <td>
+                                    <button type="button" class="btn btn-light btn-floating">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                        </svg>
+                                    </button>
+                                </td>
+                                <td></td>
+
+                            </tr>
+                            <tr>
+                                <td>0002</td>
+                                <td>Bunk bed</td>
+                                <td></td>
+                                <td>
+                                    <button type="button" class="btn btn-light btn-floating">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                        </svg>
+                                    </button>
+                                </td>
+
+                            </tr>
+                            <tr>
+                                <td>0003</td>
+                                <td>Table</td>
+                                <td></td>
+                                <td>
+                                    <button type="button" class="btn btn-light btn-floating">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                        </svg>
+                                    </button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>0004</td>
+                                <td>Table</td>
+                                <td></td>
+                                <td>
+                                    <button type="button" class="btn btn-light btn-floating">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                                            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                            <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                        </svg>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
+
+                    <div>
+                        <h2>Search Asset</h2>
+                    </div>
+
+
+
                 </div>
             </div>
         </div>

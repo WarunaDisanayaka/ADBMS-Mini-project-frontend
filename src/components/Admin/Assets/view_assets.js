79cc7f5eec0e953429fc91e4fa96945a2117c6b5
Tharukee Kiriella <tharukee99@gmail.com>
2023-11-04 16:13:09 +0530

    Asset table

--- src/components/Admin/Assets/view_assets.js
+++ src/components/Admin/Assets/view_assets.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../Sidebar';
 import Topbar from '../../Topbar';
+import DataTable from '../Rooms/DataTable';
+import AssetTable from './asset_table';
+import axios from 'axios';
 
 function ViewAssets() {
+    const columns = [
+        { label: 'Asset ID', dataKey: 'assetsId' },
+        { label: 'Asset Name', dataKey: 'name' },
+        { label: 'Asset Status', dataKey: 'status' },
+        { label: 'Room ID', dataKey: 'roomId' },
+        { label: 'Added Date', dataKey: 'addedDate' },
+        { label: 'QR Code', dataKey: 'qrCodePath' },
+
+    ];
+
+    const [assetdata, setAssetData] = useState([]);
+
+    useEffect(() => {
+        // Fetch data from the API
+        axios.get('http://3.229.95.193:8080/assets/')
+            .then(response => {
+                // Handle success
+                setAssetData(response.data);
+
+            })
+            .catch(error => {
+                // Handle error
+                console.error('Error fetching room data:', error);
+            });
+    }, []);
+
     return (
         <div className='d-flex'>
             <div>
@@ -11,9 +40,10 @@ function ViewAssets() {
             <div className='flex-grow-1'>
                 <Topbar/>
                 <div className="p-4">
-                    <h2>View Assets Page</h2>
-                    <p>This is the View Assets page content. You can add your content here.</p>
-                    
+                    <AssetTable
+                        columns={columns}
+                        data={assetdata}
+                    />
                 </div>
             </div>
         </div>

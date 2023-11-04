c80b2f1fdc6a02619b849f1b803d171be3b2d9a0
ravinduWP <104779889+ravinduWP@users.noreply.github.com>
2023-11-04 18:37:16 +0530

    Update add_assets.js

--- src/components/Admin/Assets/add_assets.js
+++ src/components/Admin/Assets/add_assets.js
@@ -15,6 +15,7 @@ function AddAssets() {
         setValidated(true);
     };
 
+
     return (
         <div className='d-flex'>
             <div>
@@ -22,42 +23,72 @@ function AddAssets() {
             </div>
             <div className='flex-grow-1'>
                 <Topbar/>
-                <div className='container p-3 mb-5 mx-auto mt-5 rounded bg-light shadow' style={{width:400}}>
+                <div className='container p-4 mb-5 mx-auto mt-5 rounded bg-light shadow' style={{ width: 500 }}>
+                    <Form noValidate validated={validated} onSubmit={handleSubmit} method='post'>
+                        <h3 className='text-center mb-4'>Add Asset</h3>
+                        <div className='row justify-content-md-center'>
+                            <div className="col-4 mb-2 p-1">
+                                <label className=''>Asset Name  </label>
+                            </div>
 
-                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                            <div className='col-6 mb-2 p-1'>
 
-                            <h3 className='text-center'>Add Assets</h3>
-                                <div className="mb-3 mt-3">
-                                    <label htmlFor='assetsId' className='form-label'>Assets ID: </label>
-                                    <div className="col-sm-10">
-                                        <Form.Control type='text' className='form-control' id='assetsId' name='assetsId' required/>
-                                    </div>
-                                </div>
-                                <div className="mb-3 mt-3">
-                                    <label htmlFor='name' className='form-label'>Assets Name: </label>
-                                    <div className="col-sm-10">
-                                        <Form.Control input type='text' className='form-control' id='name' name='name'required/>
-                                    </div>
-                                </div>
-                                <div className="mb-3 mt-3">
-                                    <label htmlFor='status' className='form-label'>Status: </label>
-                                    <div className="col-sm-10">
-                                        <Form.Select className='mt-3' required >
-                                            <option disabled selected value="">please select...</option>
-                                            <option value='in use'>In Use</option>
-                                            <option value='on repair'>On Repair</option>
-                                            <option value='out of use'>Out of use</option>
-                                        </Form.Select>
-                                    </div>
-                                </div>
-                                <div className="mb-3 mt-3">
-                                    <label htmlFor='room number' className='form-label'>Room Number: </label>
-                                    <div className="col-sm-10">
-                                        <Form.Control input type='text' className='form-control' id='room-number' name='name'required/>
-                                    </div>
-                                </div>
-                                <button type="submit" className="btn btn-primary">Submit</button>
-                                <button type="clear" className="btn btn-secondary mx-3" >Clear</button>
+                                <Form.Select required name="assetname"
+                                    // value={hostal}
+                                    // onChange={(e) => setHostal(e.target.value)}
+                                >
+                                    <option disabled selected value="">select the asset..</option>
+                                    <option value="a1">a1</option>
+                                    <option value="a2">a2</option>
+                                    <option value="a2">a3</option>
+                                </Form.Select>
+                            </div>
+                        </div>
+                        <div className='row justify-content-md-center'>
+                            <div className="col-4 mb-2 p-1">
+                                <label className=''>Status</label>
+                            </div>
+
+                            <div className='col-6 mb-2 p-1'>
+                                <Form.Select required name="status"
+                                    // value={floor}
+                                    // onChange={(e) => setFloor(e.target.value)}
+                                >
+                                    <option disabled selected value="">select the status..</option>
+                                    <option value="1">S1</option>
+                                    <option value="2">S2</option>
+                                    <option value="3">S3</option>
+                                    
+
+                                </Form.Select>
+                            </div>
+                        </div>
+                        <div className='row justify-content-md-center'>
+                            <div className="col-4 mb-2 p-1">
+                                <label  className=''>User ID</label>
+                            </div>
+
+                            <div className='col-6 mb-2 p-1'>
+
+                                <Form.Select required name="userid"
+                                    // value={roomType}
+                                    // onChange={(e) => setRoomType(e.target.value)}
+                                >
+                                    <option disabled selected value="">select the room type</option>
+                                    <option value="bedroom">Bedroom</option>
+                                    <option value="washroom">Washroom</option>
+                                    <option value="common">Common Room</option>
+                                </Form.Select>
+                            </div>
+                        </div>
+                        <div className="row justify-content-end">
+                            <div className='col-2 mb-2 p-1'>
+                                <button type='submit' className="btn btn-outline-primary">Add</button>
+                            </div>
+                            <div className='col-3 mb-2 p-1'>
+                                <button type='reset' className="btn btn-outline-secondary">Cancel</button>
+                            </div>
+                        </div>
                     </Form>
                 </div>
             </div>

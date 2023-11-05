f2cb8136802e07e06c7f406ee70a9b44d8a9d1c1
ravinduWP <104779889+ravinduWP@users.noreply.github.com>
2023-11-05 19:33:44 +0530

    update

--- src/components/Admin/Assets/add_assets.js
+++ src/components/Admin/Assets/add_assets.js
@@ -2,9 +2,20 @@ import React, { useState } from 'react';
 import Sidebar from '../Sidebar';
 import Topbar from '../../Topbar';
 import { Form } from 'react-bootstrap';
+import Select from 'react-select';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import { format } from 'date-fns';
 
 function AddAssets() {
     const [validated, setValidated] = useState(false);
+    const [name, setName] = useState([]);
+    const [roomid, setRoomId] = useState([]);
+    const [idlist, setIdList] = useState([]);
+    const status = "in use";
+
+
 
     const [value1, setValue1] = useState("");
     const [value2, setValue2] = useState("");
@@ -30,22 +41,121 @@ function AddAssets() {
     const handleSubmit = (event) => {
         const form = event.currentTarget;
         if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
+            event.preventDefault();
+            event.stopPropagation();
+            setValidated(true);
+        } else {
+            event.preventDefault();
+
+            const number1 = Math.floor(100 + Math.random() * 999);
+            const number2 = Math.floor(100 + Math.random() * 999);
+            const uniqueID = number1.toString() + number2.toString();
+
+            const now = new Date();
+            const date = format(now, 'yyyy-MM-dd');
+
+
+
+            const formdata = {
+                assetsId: uniqueID,
+                name: name.value,
+                status: status,
+                roomId: roomid.value,
+                addedDate: date
+            };
+
+            sendData(formdata);
+            setName([]);
+            setRoomId([]);
+            setValidated(false);
+
+
         }
-        setValidated(true);
+
+
     };
 
+    const handleReset = () => {
+        setName([]);
+        setRoomId([]);
+        setValidated(false);
+    };
+
+    const sendData = (data) => {
+        axios.post('http://3.229.95.193:8080/assets/', data)
+            .then(response => {
+                // Handle success
+                console.log(response);
+                notify();
+
+            })
+            .catch(error => {
+                // Handle error
+                console.error(error);
+            });
+    };
+
+    const notify = () => {
+        toast.success('Record created successfully!', {
+            position: "top-right",
+            autoClose: 4000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+
+    };
+
+    useEffect(() => {
+
+        axios.get('http://3.229.95.193:8080/rooms/get-all-room-ids')
+            .then(response => {
+                // Handle success
+                const formattedList = response.data.map(id => ({ value: id, label: id }));
+                setIdList(formattedList);
+            })
+            .catch(error => {
+                // Handle error
+                console.error('Error fetching room data:', error);
+            });
+        // console.log(idlist);
+    }, []);
+
+    const names = [
+        { value: 'table', label: 'table' },
+        { value: 'Chair', label: 'chair' },
+        { value: 'light', label: 'Light' },
+        { value: 'fan', label: 'Fan' },
+        { value: 'clothrack', label: 'Cloth_rack' },
+        { value: 'cupboard', label: 'Cupboard' },
+        { value: 'bench', label: 'Bench' },
+
+    ];
+    // const state = [
+    //     { value: 'in use', label: 'In use' },
+    //     { value: 'under maintenance', label: 'Under maintenance' },
+    //     { value: 'out of service', label: 'Out of service' },
+
+    // ];
 
     return (
         <div className='d-flex'>
             <div>
-                <Sidebar/>
+                <Sidebar />
             </div>
             <div className='flex-grow-1'>
+<<<<<<< HEAD
                 <Topbar/>
                 <div className='container p-4 mb-5 mx-auto mt-5 rounded bg-light shadow' style={{ width: 500 }}>
                     <Form noValidate validated={validated} onSubmit={handleSubmit} method='get'>
+=======
+                <Topbar />
+                <div className='container p-4 mb-5 mx-auto mt-5 rounded bg-light shadow-sm' style={{ width: 500 }}>
+                    <Form noValidate validated={validated} onSubmit={handleSubmit} method='post'>
+>>>>>>> parent of 255123a (aaa no)
                         <h3 className='text-center mb-4'>Add Asset</h3>
                         <div className='row justify-content-md-center'>
                             <div className="col-4 mb-2 p-1">
@@ -53,6 +163,7 @@ function AddAssets() {
                             </div>
 
                             <div className='col-6 mb-2 p-1'>
+<<<<<<< HEAD
 
                                 <Form.Select required name="assetname" onChange={handleChange1} value={value1}
                                     // value={hostal}
@@ -63,14 +174,29 @@ function AddAssets() {
                                     <option value="a2">a2</option>
                                     <option value="a2">a3</option>
                                 </Form.Select>
+=======
+                                <Select
+                                    isSearchable={false}
+                                    required
+                                    options={names}
+                                    value={name}
+                                    onChange={setName}
+                                />
+                                {validated && name.length === 0 && (
+                                    <div className="invalid-feedback d-block">
+                                        Please select an option.
+                                    </div>
+                                )}
+>>>>>>> parent of 255123a (aaa no)
                             </div>
                         </div>
+
                         <div className='row justify-content-md-center'>
                             <div className="col-4 mb-2 p-1">
-                                <label className=''>Status</label>
+                                <label className=''>Room ID</label>
                             </div>
-
                             <div className='col-6 mb-2 p-1'>
+<<<<<<< HEAD
                                 <Form.Select required name="status" onChange={handleChange2} value={value2}
                                     // value={floor}
                                     // onChange={(e) => setFloor(e.target.value)}
@@ -102,6 +228,20 @@ function AddAssets() {
                                     <option value="washroom">Washroom</option>
                                     <option value="common">Common Room</option>
                                 </Form.Select>
+=======
+                                <Select
+
+                                    required
+                                    options={idlist}
+                                    value={roomid}
+                                    onChange={setRoomId}
+                                />
+                                {validated && roomid.length === 0 && (
+                                    <div className="invalid-feedback d-block">
+                                        Please select an option.
+                                    </div>
+                                )}
+>>>>>>> parent of 255123a (aaa no)
                             </div>
                         </div>
                         <div className="row justify-content-end">
@@ -109,12 +249,18 @@ function AddAssets() {
                                 <button type='submit' className="btn btn-outline-primary">Add</button>
                             </div>
                             <div className='col-3 mb-2 p-1'>
+<<<<<<< HEAD
                                 <button onClick={handleClick} className="btn btn-outline-secondary">Clear</button>
+=======
+                                <button type='reset' className="btn btn-outline-secondary" onClick={handleReset}>Cancel</button>
+>>>>>>> parent of 255123a (aaa no)
                             </div>
                         </div>
                     </Form>
+
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 }

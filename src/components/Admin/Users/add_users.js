e6caa36827d9429b356a734ecfe5f8e7f9fc7566
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2023-11-05 11:25:49 +0530

    add validation

--- src/components/Admin/Users/add_users.js
+++ src/components/Admin/Users/add_users.js
@@ -29,31 +29,34 @@ function AddUsers() {
                         <h3 className='text-center'>Add User</h3>
                         <div className='mb-2 p-1'>
                             <label htmlFor='username' className='mb-2'>User name</label>
-                            <input type='text' placeholder='Username' className='form-control' />
+                            <Form.Control input type='text' placeholder='Username' className='form-control' required />
                         </div>
                         <div className='mb-2 p-1'>
                             <label htmlFor='fullrname' className='mb-2'>Full_rname</label>
-                            <input type='text' placeholder='Enter Fullrname' className='form-control' />
+                            <Form.Control input type='text' placeholder='Enter Fullrname' className='form-control'required />
                         </div>
                         <div className='mb-2 p-1'>
                             <label htmlFor='regno' className='mb-2'>Register Number</label>
-                            <input type='text' placeholder='Enter Register Number' className='form-control' />
+                            <Form.Control input type='text' placeholder='Enter Register Number' className='form-control' required />
                         </div>
                         <div className='mb-2 p-1'>
                             <label htmlFor='password' className='mb-2'>Password</label>
-                            <input type='password' placeholder='Password' className='form-control' />
+                            <Form.Control input type='password' placeholder='Password' className='form-control' required />
                         </div>
-                            <div className='mb-2 p-1'>
-                                <label htmlFor='role_id' className='mb-2'>Role ID</label>
-                                    <Form.Select className="mb-2" aria-label="Default select example">
-                                        <option selected>Role ID...</option>
+                        <div className='mb-2 p-1'>
+
+                            <label htmlFor='role_id' className='mb-2'>Role ID</label>
+
+                                    <Form.Select required className="roleId"
+                                    >
+                                        <option disabled selected value="">Role ID...</option>
                                         <option value="1">One</option>
                                         <option value="2">Two</option>
                                         <option value="3">Three</option>
-                                    </Form.Select>
-                            </div>
+                                    </Form.Select>                           
+                        </div>
                         <div className="d-grid ">
-                                <button type='submit' className="btn btn-outline-primary">Add</button>
+                            <button type='submit' className="btn btn-outline-primary">Add</button>
                             <button type='button' className="btn btn-outline-secondary mt-2">Clear</button>
                         </div>
                         

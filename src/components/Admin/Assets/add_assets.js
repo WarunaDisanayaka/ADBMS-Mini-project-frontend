ab2f15085b8527af35fde1d6648890e1daf90076
Dhananjaya Maveekumbura <80608433+dhananjaya889@users.noreply.github.com>
2023-11-05 18:50:12 +0530

    add button

--- src/components/Admin/Assets/add_assets.js
+++ src/components/Admin/Assets/add_assets.js
@@ -6,6 +6,27 @@ import { Form } from 'react-bootstrap';
 function AddAssets() {
     const [validated, setValidated] = useState(false);
 
+    const [value1, setValue1] = useState("");
+    const [value2, setValue2] = useState("");
+    const [value3, setValue3] = useState("");
+    const handleClick = (val) => {
+        setValue1("");
+        setValue2("");
+        setValue3("");
+    };
+
+    const handleChange1 = (event) => {
+        setValue1(event.target.value);
+    };
+    const handleChange2 = (event) => {
+        setValue2(event.target.value);
+    };
+    const handleChange3 = (event) => {
+        setValue3(event.target.value);
+    };
+
+    
+
     const handleSubmit = (event) => {
         const form = event.currentTarget;
         if (form.checkValidity() === false) {
@@ -24,7 +45,7 @@ function AddAssets() {
             <div className='flex-grow-1'>
                 <Topbar/>
                 <div className='container p-4 mb-5 mx-auto mt-5 rounded bg-light shadow' style={{ width: 500 }}>
-                    <Form noValidate validated={validated} onSubmit={handleSubmit} method='post'>
+                    <Form noValidate validated={validated} onSubmit={handleSubmit} method='get'>
                         <h3 className='text-center mb-4'>Add Asset</h3>
                         <div className='row justify-content-md-center'>
                             <div className="col-4 mb-2 p-1">
@@ -33,7 +54,7 @@ function AddAssets() {
 
                             <div className='col-6 mb-2 p-1'>
 
-                                <Form.Select required name="assetname"
+                                <Form.Select required name="assetname" onChange={handleChange1} value={value1}
                                     // value={hostal}
                                     // onChange={(e) => setHostal(e.target.value)}
                                 >
@@ -50,7 +71,7 @@ function AddAssets() {
                             </div>
 
                             <div className='col-6 mb-2 p-1'>
-                                <Form.Select required name="status"
+                                <Form.Select required name="status" onChange={handleChange2} value={value2}
                                     // value={floor}
                                     // onChange={(e) => setFloor(e.target.value)}
                                 >
@@ -63,6 +84,8 @@ function AddAssets() {
                                 </Form.Select>
                             </div>
                         </div>
+
+
                         <div className='row justify-content-md-center'>
                             <div className="col-4 mb-2 p-1">
                                 <label  className=''>User ID</label>
@@ -70,7 +93,7 @@ function AddAssets() {
 
                             <div className='col-6 mb-2 p-1'>
 
-                                <Form.Select required name="userid"
+                                <Form.Select required name="userid" onChange={handleChange3} value={value3}
                                     // value={roomType}
                                     // onChange={(e) => setRoomType(e.target.value)}
                                 >
@@ -86,7 +109,7 @@ function AddAssets() {
                                 <button type='submit' className="btn btn-outline-primary">Add</button>
                             </div>
                             <div className='col-3 mb-2 p-1'>
-                                <button type='reset' className="btn btn-outline-secondary">Cancel</button>
+                                <button onClick={handleClick} className="btn btn-outline-secondary">Clear</button>
                             </div>
                         </div>
                     </Form>

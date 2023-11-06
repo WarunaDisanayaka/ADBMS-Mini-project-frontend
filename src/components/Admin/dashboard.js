2526d5a393d2c54dce14de3f3d649a8deb3acf36
Waruna Disanayaka <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-11-06 23:13:15 +0530

    Fixed the conflict issue in merge (#45)

--- src/components/Admin/dashboard.js
+++ src/components/Admin/dashboard.js
@@ -1,97 +1,75 @@
-import React from 'react'
-import Topbar from '../Topbar'
-import Sidebar from './Sidebar'
-import { Card,Row,Col } from 'react-bootstrap'
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import Topbar from '../Topbar';
+import Sidebar from './Sidebar';
+import { Card, Row, Col } from 'react-bootstrap';
 import axios from 'axios';
+
 function Dashboard() {
+  const [open, setOpen] = useState('');
+  const [inprogress, setInprogress] = useState('');
+  const [repair, setRepair] = useState('');
+  const [closed, setClosed] = useState('');
 
-  const [open,setOpen] = useState('');
-  const [inprogress,setInprogress] = useState('');
-  const [repair,setRepair] = useState('');
-  const [closed,setClosed] = useState(''); 
-
-  const variant = ['danger','info','warning','success'];
-  const titles = ['Total Open Complains','Total Reviewed Cases','Total on Repair cases','Total Reslove Cases'];
-  const numbers =[open,inprogress,repair,closed];
-  const iconlist = ['fas fa-exclamation','fas fa-user-check','fas fa-wrench','fas fa-check'];
-
-  
-
-  useEffect(()=>{
+  useEffect(() => {
     axios.post('http://3.229.95.193:8080/complains/openCount')
-            .then(response => {
-                // Handle success
-                setOpen(response.data);
-               
-            })
-            .catch(error => {
-                // Handle error
-              console.log(error);
-            });
-            axios.post('http://3.229.95.193:8080/complains/inProgressCount')
-            .then(response => {
-                // Handle success
-                setInprogress(response.data);
-               
-            })
-            .catch(error => {
-                // Handle error
-              console.log(error);
-            });
-            axios.post('http://3.229.95.193:8080/complains/repairCount')
-            .then(response => {
-                // Handle success
-                setRepair(response.data);
-               
-            })
-            .catch(error => {
-                // Handle error
-              console.log(error);
-            });
-            axios.post('http://3.229.95.193:8080/complains/closedCount')
-            .then(response => {
-                // Handle success
-                setClosed(response.data);
-               
-            })
-            .catch(error => {
-                // Handle error
-              console.log(error);
-            });
-    
-  },[]);
+      .then((response) => {
+        setOpen(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-function Dashboard() {
+    axios.post('http://3.229.95.193:8080/complains/inProgressCount')
+      .then((response) => {
+        setInprogress(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-  const variant = ['danger','info','warning','success'];
-  const titles = ['Total Complains','Total Reviewed Cases','Total on Repair cases','Total Reslove Cases']
-  const numbers =[25,15,3,7];
-  const iconlist = ['fas fa-exclamation','fas fa-user-check','fas fa-wrench','fas fa-check']
+    axios.post('http://3.229.95.193:8080/complains/repairCount')
+      .then((response) => {
+        setRepair(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios.post('http://3.229.95.193:8080/complains/closedCount')
+      .then((response) => {
+        setClosed(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const variant = ['danger', 'info', 'warning', 'success'];
+  const titles = ['Total Complains', 'Total Reviewed Cases', 'Total on Repair cases', 'Total Reslove Cases'];
+  const numbers = [open, inprogress, repair, closed];
+  const iconlist = ['fas fa-exclamation', 'fas fa-user-check', 'fas fa-wrench', 'fas fa-check'];
+
   return (
-
-    <div className='d-flex'>
+    <div className="d-flex">
       <div>
         <Sidebar />
       </div>
-      <div className='flex-grow-1'>
+      <div className="flex-grow-1">
         <Topbar />
         <div className="p-4 mt-5">
-        <Row xs={1} md={4} className='g-4'>
+          <Row xs={1} md={4} className="g-4">
             {[0, 1, 2, 3].map((index) => (
               <Col key={index}>
                 <Card
                   bg={variant[index]}
                   text={variant[index] === 'light' ? 'dark' : 'white'}
-                  style={{ width: '18 rem',boxShadow: '0 4px 8px rgba(0, 0, 0, 0.4)'}}
-                  className='mb-2 '
+                  style={{ width: '18 rem', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.4)' }}
+                  className="mb-2"
                 >
-                  <Card.Body >
-                    <Card.Title style={{fontWeight:'bolder'}}>{titles[index]}</Card.Title>
+                  <Card.Body>
+                    <Card.Title style={{ fontWeight: 'bolder' }}>{titles[index]}</Card.Title>
                     <Card.Text style={{ fontSize: '40px' }}>
-                        {numbers[index]} <i className={iconlist[index]} style={{ marginLeft: '140px' }}></i>
-                       
+                      {numbers[index]} <i className={iconlist[index]} style={{ marginLeft: '140px' }}></i>
                     </Card.Text>
                     for this month ...
                   </Card.Body>
@@ -100,15 +78,9 @@ function Dashboard() {
             ))}
           </Row>
         </div>
-      </div >
-
-
-
-
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default Dashboard
-
+export default Dashboard;

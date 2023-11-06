e36900eb728c1255f3a8db52b4f634910673e3b1
Ravindu Saputhanthri <104779889+ravinduWP@users.noreply.github.com>
2023-11-06 23:05:53 +0530

    Dashboard and maintanance (#44)
    * dashboard
    
    * Update sb-admin-2.min.css
    
    * count call
    
    ---------
    
    Co-authored-by: Waruna Disanayaka <86902580+WarunaDisanayaka@users.noreply.github.com>

--- src/components/Admin/Maintenance/view_maintenance.js
+++ src/components/Admin/Maintenance/view_maintenance.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sidebar from '../Sidebar';
 import Topbar from '../../Topbar';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import MaintenanceTable from './maintanance_table';
+
 
 function ViewMaintenance() {
+    const columns = [
+        { label: 'Asset ID', dataKey: 'username' },
+        { label: 'Status', dataKey: 'fullName' },
+        { label: 'Registration NO', dataKey: 'regNo' },
+        { label: 'Role', dataKey: 'roleId' }
+        
+        
+
+    ];
+
+
+    const [userdata, setUserData] = useState([]);
+
+    const notify = () => {
+        toast.error("Error occurred.", {
+          position: 'top-right',
+          autoClose: 4000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'light',
+        });
+      };
+
+    useEffect(() => {
+        // Fetch data from the API
+        axios.get('http://3.229.95.193:8080/users/get-users')
+            .then(response => {
+                // Handle success
+                setUserData(response.data);
+
+            })
+            .catch(error => {
+                // Handle error
+              notify();
+            });
+    }, []);
+
+
+
     return (
         <div className='d-flex'>
             <div>
@@ -11,12 +57,19 @@ function ViewMaintenance() {
             <div className='flex-grow-1'>
                 <Topbar/>
                 <div className="p-4">
-                    <h2>View Maintenance Page</h2>
-                    <p>This is the View Maintenance page content. You can add your content here.</p>
+                    <MaintenanceTable
+                        columns={columns}
+                        data={userdata}
+                    />
                 </div>
             </div>
         </div>
     );
 }
 
+
+
 export default ViewMaintenance;
+
+
+
